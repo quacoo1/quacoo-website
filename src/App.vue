6160ee1ecc22c1d78baccd795390b497ba2ae8c9
Quacoo <quacoo.boat@gmail.com>
2022-03-02 17:56:46 +0000

    this is the first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+</script>
+
+<template>
+  KWAKU BOATENG'S WEBSITE
+</template>
+
+<style lang="scss"> 
+  $type_base_font: 'Roboto', sans-serif;
+  $color_grey:  #F6F7F7;
+  $color_primary: #3C2BFE;
+  $color_secondary: #2DCAED;
+  $color_tertiary: #151B24;
+  $color_background: $color_tertiary;
+  
+  #app{
+    color: $color_grey;
+    font:100% $type_base_font;
+    background: $color_background;
+    min-height:100%;
+  }
+</style>
